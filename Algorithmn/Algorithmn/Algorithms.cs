874efb89c2a34ebf1f397d7f7ca6b919411d0d4c
rohantare03 <rohatare123@gmail.com>
2022-05-28 20:23:45 +0530

    [Rohan] Add : Sorting values using Insertion Sort

---
+++ Algorithmn/Algorithmn/Algorithms.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    internal class Algorithms
+    {
+        public void Insert()
+        {
+            string[] names = { "Rohan", "Xerbes", "Ben", "Pauli", "Mark" };
+            InsertSort(names);
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static void InsertSort(IComparable[] array)
+        {
+            int i, j;
+
+            for (i = 1; i < array.Length; i++)
+            {
+
+                IComparable value = array[i];
+                j = i - 1;
+                while ((j >= 0) && (array[j].CompareTo(value) > 0))
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+                array[j + 1] = value;
+            }
+        }
+    }
+}
---
+++ Algorithmn/Algorithmn/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Algorithm
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Algorithms algorithms = new Algorithms();
+            algorithms.Insert();
+        }
+    }
+}
